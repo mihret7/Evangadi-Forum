d8d8235bd01cbc3603183f0e2fa612fcde5f0c0f
Yo-eden <eddyyo15@yahoo.com>
2025-06-11 06:14:04 -0700

    updated

--- server/routes/getquestions.js
+++ server/routes/getquestions.js
@@ -11,7 +11,7 @@ router.get("/all-question", async(req, res)=>{
         const [questions]= await dbconnection.query("SELECT q.questions_id, q.question_title, q.question_discription, q.tag, q.user+id, r.user_name, FROM question q JOIN registration r ON q.user_id=r.user_id")
         res.status(StatusCodes.ACCEPTED).json({questions})
     }catch (error){
-        res.status(500).json({message: "error retrieving question", error:error.message})
+        res.status(StatusCodes.INTERNAL_SERVER_ERROR).json({message: "error retrieving question", error:error.message})
     }
 })
 
