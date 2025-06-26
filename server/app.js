8de545d9502afe8ac42136d5242991779f487d26
HakimcHuu <168078248+HakimcHuu@users.noreply.github.com>
2025-06-26 21:01:08 +0300

    Update app.js

--- server/app.js
+++ server/app.js
@@ -20,7 +20,7 @@ app.use(
       "http://localhost:4321",
       "http://localhost:5173",
       "http://localhost:3000",
-      "https://evangadi-forum-beta7.vercel.app",
+      // "https://evangadi-forum-beta7.vercel.app",
       "https://evangadi-forum-front-end-six.vercel.app/home"
     ],
     credentials: true,
